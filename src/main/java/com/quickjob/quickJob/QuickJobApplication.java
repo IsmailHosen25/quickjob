e47977a6559d2ae708e14f6927697afed364d882
Md. Ismail Hosen <ismailhosen1006@gmail.com>
2025-05-17 19:34:31 +0600

    quick job app started

---
+++ src/main/java/com/quickjob/quickJob/QuickJobApplication.java
@@ -0,0 +1,13 @@
+package com.quickjob.quickJob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickJobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quickjob/quickJob/QuickJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickjob.quickJob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
